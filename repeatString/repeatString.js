116231ec9749dfaa7a15573259b25313cbdb0465
Alain Tran <alaint77@gmail.com>
2019-04-03 15:07:13 +0200

    Finished helloWord et repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+  if (times > 0)
+    return string.repeat(times);
+  else if (times < 0)
+    return "ERROR";
+  else
+    return "";
 }
 
 module.exports = repeatString
