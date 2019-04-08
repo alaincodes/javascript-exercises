674054db15cfd6166682ac12bcd454ffe1b3f636
Alain Tran <alaint77@gmail.com>
2019-04-08 17:10:19 +0200

    Finished FIB with better understanding style

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,10 +1,13 @@
-const fibonacci = function(n) {
-  if (n < 0) return "OOPS";
-  let arr = [0, 1];
-  for (let i = 2; i < n + 1; i++){
-    arr.push(arr[i - 2] + arr[i -1])
+const fibonacci = function(number) {
+  if (number < 0) return "OOPS";
+  let previous_first = 0, previous_second = 1, next = 1;
+
+  for(let i = 2; i <= number; i++) {
+      next = previous_first + previous_second;
+      previous_first = previous_second;
+      previous_second = next;
   }
- return arr[n]
+  return next;
 }
 
 module.exports = fibonacci
