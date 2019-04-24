eca694085845b9f79d7fad4b2937e859cf838f9b
Alain <alaint77@gmail.com>
2019-04-24 12:13:25 +0200

    finished version

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let result = (fahrenheit - 32) * 5/9;
+  return Math.round(result * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let result = celcius * 9/5 + 32;
+  return Math.round(result * 10) / 10;
 }
 
 module.exports = {
