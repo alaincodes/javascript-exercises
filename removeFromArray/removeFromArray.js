2cdd0d5610ab01c01167beeeb61039d8e635715c
Alain <alaint77@gmail.com>
2019-04-24 15:58:08 +0200

    fiiinsiehdddeedede

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...theArgs) {
+  const arr = theArgs[0]
+   return arr.filter(value => !theArgs.includes(value));
 }
 
 module.exports = removeFromArray
